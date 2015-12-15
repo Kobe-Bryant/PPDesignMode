560ff75c5fb95e0cf6c0ce9cbde2413126ee742d
feng.ma <feng.ma@patpat.com>
2015-12-15 11:13:22 +0800

    update scrollTopButton

---
+++ PPDesignMode/PPDesignMode/PPGameRole.h
@@ -0,0 +1,14 @@
+//
+//  PPGameRole.h
+//  PPDesignMode
+//
+//  Created by patpat on 15/12/14.
+//  Copyright © 2015年 test. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PPGameRole : NSObject
+
+
+@end
---
+++ PPDesignMode/PPDesignMode/PPGameRole.m
@@ -0,0 +1,13 @@
+//
+//  PPGameRole.m
+//  PPDesignMode
+//
+//  Created by patpat on 15/12/14.
+//  Copyright © 2015年 test. All rights reserved.
+//
+
+#import "PPGameRole.h"
+
+@implementation PPGameRole
+
+@end
