cac3f4750136fb0be541fff0fc109ab000441ca9
feng.ma <feng.ma@patpat.com>
2016-01-05 16:50:16 +0800

    update

---
+++ PPDesignMode/PPDesignMode/命令模式/PPWaiter.h
@@ -0,0 +1,13 @@
+//
+//  PPWaiter.h
+//  PPDesignMode
+//
+//  Created by patpat on 16/1/5.
+//  Copyright © 2016年 test. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PPWaiter : NSObject
+
+@end
---
+++ PPDesignMode/PPDesignMode/命令模式/PPWaiter.m
@@ -0,0 +1,13 @@
+//
+//  PPWaiter.m
+//  PPDesignMode
+//
+//  Created by patpat on 16/1/5.
+//  Copyright © 2016年 test. All rights reserved.
+//
+
+#import "PPWaiter.h"
+
+@implementation PPWaiter
+
+@end
